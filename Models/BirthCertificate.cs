9d640b31e7fe91e104ffa610f892956987f6eb14
Cheleloh <jane062001@icloud.com>
2025-09-17 16:36:08 +0200

    Add BirthCertificate model

---
+++ Models/BirthCertificate.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BirthCertificateRegistrationSystem.Models
+{
+    public class BirthCertificate
+    {
+        public int Id { get; set; }
+        public string CertificateNumber { get; set; }
+        public string ChildName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PlaceOfBirth { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+        public DateTime DateIssued { get; set; }
+        public int RegisteredByUserId { get; set; }
+    }
+}
