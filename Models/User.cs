d319900cde4d20b9c05d00c0a756cd0e3a8f7876
Cheleloh <jane062001@icloud.com>
2025-09-17 16:31:23 +0200

    Add User model

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BirthCertificateRegistrationSystem.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
